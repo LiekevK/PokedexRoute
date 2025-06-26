4d1c5a30df618d55b76f5585f78d2f13a366e524
Jaron Zijlstra <support@jaronline.dev>
2025-06-26 20:41:47 +0200

    Initial commit

---
+++ src/main/java/liekevk/pokedexroute/PokedexRouteApplication.java
@@ -0,0 +1,13 @@
+package liekevk.pokedexroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexRouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokedexRouteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/liekevk/pokedexroute/PokedexRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package liekevk.pokedexroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexRouteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
